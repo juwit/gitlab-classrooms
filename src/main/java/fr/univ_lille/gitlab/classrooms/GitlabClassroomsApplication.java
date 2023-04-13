222bcc1af85838fece921c71390ea4a8d903d39e
Julien WITTOUCK <julien.wittouck@gmail.com>
2023-04-13 18:23:26 +0200

    🎉 : init project

---
+++ src/main/java/fr/univ_lille/gitlab/classrooms/GitlabClassroomsApplication.java
@@ -0,0 +1,13 @@
+package fr.univ_lille.gitlab.classrooms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitlabClassroomsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitlabClassroomsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/univ_lille/gitlab/classrooms/GitlabClassroomsApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.univ_lille.gitlab.classrooms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitlabClassroomsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
