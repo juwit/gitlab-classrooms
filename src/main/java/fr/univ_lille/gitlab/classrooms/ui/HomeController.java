01abb55757e16d6528d36a528c4cb4f84dfa76ba
Julien WITTOUCK <julien.wittouck@gmail.com>
2023-05-10 16:06:08 +0200

    fixup! ♻ : extract Classroom to its own class file

--- src/main/java/fr/univ_lille/gitlab/classrooms/ui/HomeController.java
+++ src/main/java/fr/univ_lille/gitlab/classrooms/ui/HomeController.java
@@ -1,5 +1,6 @@
 package fr.univ_lille.gitlab.classrooms.ui;
 
+import fr.univ_lille.gitlab.classrooms.domain.Classroom;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
