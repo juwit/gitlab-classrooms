026d1d06838d53c58d29b1b4f9a00f9d497d5b4e
Julien WITTOUCK <julien.wittouck@gmail.com>
2023-05-10 17:40:30 +0200

    🔥 : remove unused imports

--- src/main/java/fr/univ_lille/gitlab/classrooms/mock/ClassroomRepositoryMock.java
+++ src/main/java/fr/univ_lille/gitlab/classrooms/mock/ClassroomRepositoryMock.java
@@ -5,9 +5,7 @@ import fr.univ_lille.gitlab.classrooms.domain.ClassroomRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 
 @Repository
 class ClassroomRepositoryMock implements ClassroomRepository {
