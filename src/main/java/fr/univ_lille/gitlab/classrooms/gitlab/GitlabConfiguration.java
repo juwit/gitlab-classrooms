9ce4f3e3a56a0710403a971acd05f63b46acb8a6
Julien WITTOUCK <julien.wittouck@gmail.com>
2023-05-16 16:52:36 +0200

    ✨ : use OAuth2AuthorizedClient to authenticate to the gitlab Api

--- src/main/java/fr/univ_lille/gitlab/classrooms/gitlab/GitlabConfiguration.java
+++ src/main/java/fr/univ_lille/gitlab/classrooms/gitlab/GitlabConfiguration.java
@@ -1,21 +1,35 @@
 package fr.univ_lille.gitlab.classrooms.gitlab;
 
+import org.gitlab4j.api.Constants;
 import org.gitlab4j.api.GitLabApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 
 @Configuration
 public class GitlabConfiguration {
 
     private GitlabProperties gitlabProperties;
 
-    public GitlabConfiguration(GitlabProperties gitlabProperties) {
+    private OAuth2AuthorizedClientService oAuth2AuthorizedClientService;
+
+    public GitlabConfiguration(GitlabProperties gitlabProperties, OAuth2AuthorizedClientService oAuth2AuthorizedClientService) {
         this.gitlabProperties = gitlabProperties;
+        this.oAuth2AuthorizedClientService = oAuth2AuthorizedClientService;
     }
 
     @Bean
-    GitLabApi gitLabApi() {
-        return new GitLabApi(gitlabProperties.url(), gitlabProperties.accessToken());
+    @Primary
+    GitLabApi oauth2GitlabApi() {
+        var client = new GitLabApi(gitlabProperties.url(), Constants.TokenType.OAUTH2_ACCESS, "");
+        client.setAuthTokenSupplier(() -> {
+            var authentication = SecurityContextHolder.getContext().getAuthentication();
+            var authorizedClient = oAuth2AuthorizedClientService.loadAuthorizedClient("gitlab", authentication.getName());
+            return authorizedClient.getAccessToken().getTokenValue();
+        });
+        return client;
     }
 
 }
