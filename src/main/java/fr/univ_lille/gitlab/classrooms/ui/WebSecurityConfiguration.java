b85107457edc37160032bc6964e7abe4ea334653
Julien WITTOUCK <julien.wittouck@gmail.com>
2023-06-01 16:27:18 +0200

    🔧 : configure storage of authorized clients

--- src/main/java/fr/univ_lille/gitlab/classrooms/ui/WebSecurityConfiguration.java
+++ src/main/java/fr/univ_lille/gitlab/classrooms/ui/WebSecurityConfiguration.java
@@ -2,12 +2,16 @@ package fr.univ_lille.gitlab.classrooms.ui;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
+import org.springframework.security.oauth2.client.JdbcOAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.core.user.OAuth2UserAuthority;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -56,6 +60,11 @@ public class WebSecurityConfiguration implements WebMvcConfigurer {
 
             return mappedAuthorities;
         };
+    }
+
+    @Bean
+    OAuth2AuthorizedClientService oAuth2AuthorizedClientService(JdbcOperations jdbcOperations, ClientRegistrationRepository clientRegistrationRepository){
+        return new JdbcOAuth2AuthorizedClientService(jdbcOperations, clientRegistrationRepository);
     }
 
 }
