537e8d293f499b6e24b0e235d05d495359dce811
Julien WITTOUCK <julien.wittouck@gmail.com>
2023-08-01 17:31:49 +0200

    ✨ : add user profile

---
+++ src/main/java/fr/univ_lille/gitlab/classrooms/ui/UserControllerAdvice.java
@@ -0,0 +1,20 @@
+package fr.univ_lille.gitlab.classrooms.ui;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.Optional;
+
+@ControllerAdvice
+public class UserControllerAdvice {
+
+    @ModelAttribute
+    void addUser(Model model, Authentication authentication){
+        if(authentication.isAuthenticated() ){
+            model.addAttribute("user", authentication.getPrincipal());
+        }
+    }
+
+}
