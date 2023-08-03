e4a09d2c74cd583cf498b5b9762ce8dd163a2351
Julien WITTOUCK <julien.wittouck@gmail.com>
2023-08-03 11:53:34 +0200

    ✅ : test role enforcement for home page

---
+++ src/test/java/fr/univ_lille/gitlab/classrooms/ui/HomeControllerMVCTest.java
@@ -0,0 +1,50 @@
+package fr.univ_lille.gitlab.classrooms.ui;
+
+import fr.univ_lille.gitlab.classrooms.domain.ClassroomRepository;
+import fr.univ_lille.gitlab.classrooms.domain.ClassroomRole;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class HomeControllerMVCTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ClassroomRepository classroomRepository;
+
+    @Nested
+    @WithMockClassroomUser(username = "obiwan.kenobi", roles = {ClassroomRole.TEACHER})
+    class TeacherRole {
+
+        @Test
+        void shouldAccessHomePage() throws Exception {
+            mockMvc.perform(get("/"))
+                    .andDo(print())
+                    .andExpect(status().isOk());
+        }
+    }
+
+    @Nested
+    @WithMockClassroomUser(username = "luke.skywalker", roles = {ClassroomRole.STUDENT})
+    class StudentRole {
+
+        @Test
+        void shouldNotAccessHomePage() throws Exception {
+            mockMvc.perform(get("/"))
+                    .andDo(print())
+                    .andExpect(status().isForbidden());
+        }
+    }
+}
