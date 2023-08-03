50985ef99600d9ea6dcd7a0522c3c0f72c54c851
Julien WITTOUCK <julien.wittouck@gmail.com>
2023-08-03 11:01:06 +0200

    🔥 : remove this unused import 'java.util.Optional'.
    https://sonarcloud.io/project/issues?open=AYm6lyd8-39H0B0JhPIz&id=gitlab-classrooms_gitlab-classrooms

--- src/main/java/fr/univ_lille/gitlab/classrooms/ui/UserControllerAdvice.java
+++ src/main/java/fr/univ_lille/gitlab/classrooms/ui/UserControllerAdvice.java
@@ -5,8 +5,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
-import java.util.Optional;
-
 @ControllerAdvice
 public class UserControllerAdvice {
 
