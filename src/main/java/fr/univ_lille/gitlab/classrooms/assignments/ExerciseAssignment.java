ee684d712ec3bf04fc0d3ef4194d049886ca8888
Julien WITTOUCK <julien.wittouck@gmail.com>
2023-08-17 16:38:56 +0200

    🔥 : remove unnecessary import

--- src/main/java/fr/univ_lille/gitlab/classrooms/assignments/ExerciseAssignment.java
+++ src/main/java/fr/univ_lille/gitlab/classrooms/assignments/ExerciseAssignment.java
@@ -1,7 +1,5 @@
 package fr.univ_lille.gitlab.classrooms.assignments;
 
-import fr.univ_lille.gitlab.classrooms.assignments.Assignment;
-import fr.univ_lille.gitlab.classrooms.assignments.AssignmentType;
 import jakarta.persistence.Entity;
 
 @Entity
