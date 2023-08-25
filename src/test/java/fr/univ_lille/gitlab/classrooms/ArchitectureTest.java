e78b7cbf3d7c4c0aefd56bfadbcb627699fc4f8e
Julien WITTOUCK <julien.wittouck@gmail.com>
2023-08-25 11:27:40 +0200

    ♻ : rewrite using @ArchTest

--- src/test/java/fr/univ_lille/gitlab/classrooms/ArchitectureTest.java
+++ src/test/java/fr/univ_lille/gitlab/classrooms/ArchitectureTest.java
@@ -1,40 +1,28 @@
 package fr.univ_lille.gitlab.classrooms;
 
-import com.tngtech.archunit.core.importer.ClassFileImporter;
-import org.junit.jupiter.api.Test;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
 import org.springframework.data.repository.Repository;
 
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 
+@AnalyzeClasses(packages = "fr.univ_lille.gitlab.classrooms")
 class ArchitectureTest {
 
-    @Test
-    void repositoriesAreNotExposedOutsidePackages() {
-        var importedClasses = new ClassFileImporter()
-                .importPackages("fr.univ_lille.gitlab.classrooms");
+    @ArchTest
+    final ArchRule repositoriesAreNotExposedOutsidePackages = classes()
+            .that()
+            .areAssignableTo(Repository.class)
+            .should()
+            .bePackagePrivate();
 
-        var rule = classes()
-                .that()
-                .areAssignableTo(Repository.class)
-                .should()
-                .bePackagePrivate();
-
-        rule.check(importedClasses);
-    }
-
-    @Test
-    void serviceInterfacesAreExposedAsPublic() {
-        var importedClasses = new ClassFileImporter()
-                .importPackages("fr.univ_lille.gitlab.classrooms");
-
-        var rule = classes()
-                .that()
-                .haveNameMatching(".*Service")
-                .should()
-                .beInterfaces()
-                .andShould()
-                .bePublic();
-
-        rule.check(importedClasses);
-    }
+    @ArchTest
+    final ArchRule serviceInterfacesAreExposedAsPublic = classes()
+            .that()
+            .haveNameMatching(".*Service")
+            .should()
+            .beInterfaces()
+            .andShould()
+            .bePublic();
 }
