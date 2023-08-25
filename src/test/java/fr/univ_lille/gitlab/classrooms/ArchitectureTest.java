5b0e01b4b7aceafbee27771844c476b6334bde63
Julien WITTOUCK <julien.wittouck@gmail.com>
2023-08-25 11:18:43 +0200

    ✅ : add repository visibility rule

---
+++ src/test/java/fr/univ_lille/gitlab/classrooms/ArchitectureTest.java
@@ -0,0 +1,24 @@
+package fr.univ_lille.gitlab.classrooms;
+
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import org.junit.jupiter.api.Test;
+import org.springframework.data.repository.Repository;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+
+class ArchitectureTest {
+
+    @Test
+    void repositoriesAreNotExposedOutsidePackages() {
+        var importedClasses = new ClassFileImporter()
+                .importPackages("fr.univ_lille.gitlab.classrooms");
+
+        var rule = classes()
+                .that()
+                .areAssignableTo(Repository.class)
+                .should()
+                .bePackagePrivate();
+
+        rule.check(importedClasses);
+    }
+}
