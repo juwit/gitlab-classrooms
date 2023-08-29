7f7fa084740dff0a5920a426a6b3a01199ac909f
Julien WITTOUCK <julien.wittouck@gmail.com>
2023-08-29 17:18:25 +0200

    ✅ : add a test with a surefire report

--- src/test/java/fr/univ_lille/gitlab/classrooms/assignments/testReports/TestSuiteParsingTest.java
+++ src/test/java/fr/univ_lille/gitlab/classrooms/assignments/testReports/TestSuiteParsingTest.java
@@ -10,10 +10,10 @@ import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.*;
 
-class TestSuiteTest {
+class TestSuiteParsingTest {
 
     @Test
-    void testReadXmlJaxb() throws IOException, JAXBException {
+    void testReadCucumberOuptut() throws IOException, JAXBException {
         var unmarshaller = JAXBContext.newInstance(TestSuite.class).createUnmarshaller();
 
         var file = new ClassPathResource("/cucumber-junit-output.xml");
@@ -42,5 +42,32 @@ class TestSuiteTest {
         assertThat(secondTest.getFailure().get(0).getValue()).contains("java.lang.AssertionError: Element with name 'password_verify' not present");
     }
 
+    @Test
+    void testReadSurefireOutput() throws IOException, JAXBException {
+        var unmarshaller = JAXBContext.newInstance(TestSuite.class).createUnmarshaller();
 
+        var file = new ClassPathResource("/TEST-test.dao.CatalogDaoTest.xml");
+
+        var root = unmarshaller.unmarshal(new StreamSource(file.getInputStream()), TestSuite.class);
+        var testSuite = root.getValue();
+
+        assertThat(testSuite.getName()).isEqualTo("test.dao.CatalogDaoTest");
+        assertThat(testSuite.getTime()).isEqualTo(0.274f);
+        assertThat(testSuite.getTests()).isEqualTo("10");
+        assertThat(testSuite.getSkipped()).isEqualTo("0");
+        assertThat(testSuite.getFailures()).isEqualTo("0");
+        assertThat(testSuite.getErrors()).isEqualTo("0");
+
+        assertThat(testSuite.getTestcase()).hasSize(10);
+        var firstTest = testSuite.getTestcase().get(0);
+        assertThat(firstTest.getClassname()).isEqualTo("test.dao.CatalogDaoTest");
+        assertThat(firstTest.getName()).isEqualTo("test_getArticleCategories_match");
+        assertThat(firstTest.getFailure()).isNullOrEmpty();
+
+        var secondTest = testSuite.getTestcase().get(1);
+        assertThat(secondTest.getClassname()).isEqualTo("test.dao.CatalogDaoTest");
+        assertThat(secondTest.getName()).isEqualTo("test_getCategories");
+        assertThat(secondTest.getFailure()).isNullOrEmpty();
+    }
+    
 }
