4f3e91d1a35b0c0fa817ddc3ad0a3b4b039a9f08
Julien WITTOUCK <julien.wittouck@gmail.com>
2023-09-05 11:05:55 +0200

    🐛 : allow the advice to the error controller

--- src/main/java/fr/univ_lille/gitlab/classrooms/users/UserControllerAdvice.java
+++ src/main/java/fr/univ_lille/gitlab/classrooms/users/UserControllerAdvice.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import java.security.Principal;
 import java.util.NoSuchElementException;
 
-@ControllerAdvice(basePackages = "fr.univ_lille.gitlab.classrooms")
+@ControllerAdvice({"fr.univ_lille.gitlab.classrooms", "org.springframework.boot.autoconfigure.web.servlet.error"})
 public class UserControllerAdvice {
 
     private final ClassroomUserService classroomUserService;
