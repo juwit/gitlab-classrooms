ff461434075b0979246481aae2e84e68c27b0d67
Julien WITTOUCK <julien.wittouck@gmail.com>
2023-11-10 15:34:48 +0100

    ✨ : add resetGrades() method on StudentAssignment

--- src/test/java/fr/univ_lille/gitlab/classrooms/assignments/StudentExerciseAssignmentTest.java
+++ src/test/java/fr/univ_lille/gitlab/classrooms/assignments/StudentExerciseAssignmentTest.java
@@ -3,6 +3,8 @@ package fr.univ_lille.gitlab.classrooms.assignments;
 import fr.univ_lille.gitlab.classrooms.assignments.grading.junit.JUnitAssignmentGrade;
 import org.junit.jupiter.api.Test;
 
+import java.time.ZonedDateTime;
+
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -20,5 +22,28 @@ class StudentExerciseAssignmentTest {
         studentExerciseAssignment.getAssignmentGrades().add(anotherJunitGrade);
 
         assertThat(studentExerciseAssignment.getAssignmentGrades()).hasSize(1);
+    }
+
+    @Test
+    void resetGrade_shouldRemoveAllTheGrades(){
+        var studentExerciseAssignment = new StudentExerciseAssignment();
+
+        var junitGrade = new JUnitAssignmentGrade();
+        studentExerciseAssignment.getAssignmentGrades().add(junitGrade);
+
+        studentExerciseAssignment.resetGrades();
+
+        assertThat(studentExerciseAssignment.getAssignmentGrades()).isEmpty();
+    }
+
+    @Test
+    void resetGrade_shouldSetTheAssignmentUnSubmitted(){
+        var studentExerciseAssignment = new StudentExerciseAssignment();
+
+        studentExerciseAssignment.setSubmissionDate(ZonedDateTime.now());
+
+        studentExerciseAssignment.resetGrades();
+
+        assertThat(studentExerciseAssignment.hasBeenSubmitted()).isFalse();
     }
 }
