1529e8961aeab83ba82b16c166f61bf364eb2331
Julien WITTOUCK <julien.wittouck@gmail.com>
2023-11-10 18:33:26 +0100

    ✅ : use correct test user

--- src/test/java/fr/univ_lille/gitlab/classrooms/assignments/StudentAssignmentViewControllerMVCTest.java
+++ src/test/java/fr/univ_lille/gitlab/classrooms/assignments/StudentAssignmentViewControllerMVCTest.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -39,7 +38,7 @@ class StudentAssignmentViewControllerMVCTest {
     }
 
     @Test
-    @WithMockUser("leai.organa")
+    @WithMockStudent(username = "leia.organa")
     void testResetGrade_asWrongStudent_shouldBeForbidden() throws Exception {
         mockMvc.perform(post("/assignments/{assignmentId}/students/{studentId}/reset", quizAssignmentId.toString(), "luke.skywalker")
                         .with(csrf()))
