612496e5fe2f4239e8c5d27390a38a5bc3f87e6b
Julien WITTOUCK <julien.wittouck@gmail.com>
2023-11-10 18:32:18 +0100

    ✅ : allow user name override

--- src/test/java/fr/univ_lille/gitlab/classrooms/users/WithMockStudent.java
+++ src/test/java/fr/univ_lille/gitlab/classrooms/users/WithMockStudent.java
@@ -1,9 +1,14 @@
 package fr.univ_lille.gitlab.classrooms.users;
 
+import org.springframework.core.annotation.AliasFor;
+
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 @Retention(RetentionPolicy.RUNTIME)
 @WithMockClassroomUser(username = "luke.skywalker", roles = {ClassroomRole.STUDENT})
 public @interface WithMockStudent {
+
+    String username() default "luke.skywalker";
+
 }
