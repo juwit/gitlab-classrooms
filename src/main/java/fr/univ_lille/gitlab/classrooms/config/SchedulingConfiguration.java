e3a824145c05d13b8b042c86d3ec88c20cdb6074
Julien WITTOUCK <julien.wittouck@gmail.com>
2024-12-30 15:44:11 +0100

    ✨ : add scheduling configuration

---
+++ src/main/java/fr/univ_lille/gitlab/classrooms/config/SchedulingConfiguration.java
@@ -0,0 +1,9 @@
+package fr.univ_lille.gitlab.classrooms.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulingConfiguration {
+}
