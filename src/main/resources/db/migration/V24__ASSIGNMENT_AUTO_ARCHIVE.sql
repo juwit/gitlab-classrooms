ed9beeb1abbf3abe1cb14e66cf2440ede49779e8
Julien WITTOUCK <julien.wittouck@gmail.com>
2024-12-30 15:43:44 +0100

    🐛 : add default value for auto_archive

--- src/main/resources/db/migration/V24__ASSIGNMENT_AUTO_ARCHIVE.sql
+++ src/main/resources/db/migration/V24__ASSIGNMENT_AUTO_ARCHIVE.sql
@@ -1,5 +1,5 @@
 ALTER TABLE assignment
-    ADD column if not exists auto_archive bool;
+    ADD column if not exists auto_archive bool default false;
 
 ALTER TABLE assignment
     ADD column if not exists due_date TIMESTAMP WITH TIME ZONE;
